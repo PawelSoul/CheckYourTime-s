28b9ac09b54304d5bc29d960a9ef4c9f78761d40
PawelSoul <soul.pawel@gmail.com>
2026-02-05 23:26:52 +0100

    poprawka

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -8,11 +8,21 @@ class AppTheme {
     useMaterial3: true,
     colorSchemeSeed: _seedLight,
     brightness: Brightness.light,
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      selectedItemColor: _seedLight,
+      unselectedItemColor: Colors.grey.shade600,
+      type: BottomNavigationBarType.fixed,
+    ),
   );
 
   static final dark = ThemeData(
     useMaterial3: true,
     colorSchemeSeed: _seedDark,
     brightness: Brightness.dark,
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      selectedItemColor: _seedDark,
+      unselectedItemColor: Colors.grey.shade400,
+      type: BottomNavigationBarType.fixed,
+    ),
   );
 }
