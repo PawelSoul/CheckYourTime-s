651895e41f0c29209d7600cf9e23a5e0622accd1
PawelSoul <soul.pawel@gmail.com>
2026-02-10 11:24:23 +0100

    poprawa trybu jasnego

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -8,6 +8,15 @@ class AppTheme {
     useMaterial3: true,
     colorSchemeSeed: _seedLight,
     brightness: Brightness.light,
+    tooltipTheme: TooltipThemeData(
+      decoration: BoxDecoration(
+        color: Colors.black87,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      textStyle: const TextStyle(
+        color: Colors.white,
+      ),
+    ),
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
       selectedItemColor: _seedLight,
       unselectedItemColor: Colors.grey.shade600,
@@ -19,6 +28,15 @@ class AppTheme {
     useMaterial3: true,
     colorSchemeSeed: _seedDark,
     brightness: Brightness.dark,
+    tooltipTheme: TooltipThemeData(
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.92),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      textStyle: const TextStyle(
+        color: Colors.black87,
+      ),
+    ),
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
       selectedItemColor: _seedDark,
       unselectedItemColor: Colors.grey.shade400,
